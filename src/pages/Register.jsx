73e11f36e7e9ab8081e754214f83fd281b3f7022
Fatemeh Gharche <fatemehgharche8@gmail.com>
2025-05-15 15:00:39 +0330

    Conecting YUP

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,57 +1,66 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 
 function Register() {
+
+    const schema = yup.object().shape({
+    username: yup.string().required('نام کاربری الزامی است.'),
+    password: yup
+    .string()
+    .required('رمز عبور الزامی است.')
+    .min(6, 'رمز عبور باید حداقل ۶ کاراکتر باشد.'),
+     repeatPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'رمز عبور و تکرار آن باید یکسان باشند.')
+    .required('تکرار رمز عبور الزامی است.'),
+});
 
     const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
-    if (data.password !== data.repeatPassword) {
-      alert('رمز عبور و تکرار آن یکسان نیستند!');
-      return;
-    }
-
     console.log('Form Data:', data);
     alert('ثبت‌نام با موفقیت انجام شد!');
   };
 
 
-
-    console.log('Form Data:', data);
-    alert('ثبت‌نام با موفقیت انجام شد!')
-
   return (
-    <div>
-      <div style={{ maxWidth: '400px', margin: 'auto' }}>
+     <div style={{ maxWidth: '400px', margin: 'auto' }}>
       <h2>ثبت‌نام</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>نام کاربری:</label>
-          <input type="text" {...register('username', { required: true })} />
-          {errors.username && <p style={{ color: 'red' }}>نام کاربری الزامی است.</p>}
+          <input type="text" {...register('username')} />
+          {errors.username && <p style={{ color: 'red' }}>{errors.username.message}</p>}
         </div>
+
         <div>
           <label>رمز عبور:</label>
-          <input type="password" {...register('password', { required: true })} />
-          {errors.password && <p style={{ color: 'red' }}>رمز عبور الزامی است.</p>}
+          <input type="password" {...register('password')} />
+          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
         </div>
+
         <div>
           <label>تکرار رمز عبور:</label>
-          <input type="password" {...register('repeatPassword', { required: true })} />
-          {errors.repeatPassword && <p style={{ color: 'red' }}>تکرار رمز عبور الزامی است.</p>}
+          <input type="password" {...register('repeatPassword')} />
+          {errors.repeatPassword && (
+            <p style={{ color: 'red' }}>{errors.repeatPassword.message}</p>
+          )}
         </div>
+
         <button type="submit">ثبت‌نام</button>
       </form>
       <p>
         قبلاً ثبت‌نام کردی؟ <a href="/login">ورود</a>
       </p>
-    </div>
     </div>
   )
 }
